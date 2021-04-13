6b6d3807fade2b93fa3172f62f10d88a7a9d0a23
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-04-13 16:58:08 +0200

    added chrome debug

--- website/src/lib/components/Retraction.svelte
+++ website/src/lib/components/Retraction.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-	import { AlgorithmV1 } from './../typescript/algorithm_v1.ts';
 	import { NumberInput, Button } from 'carbon-components-svelte';
+	import { AlgorithmV1 } from '../typescript/algorithm_v1';
 	// @ts-ignore
 	import { Column, Row } from 'svelte-fluttered';
-	import { onMount } from 'svelte';
 
-	let algoV1: AlgorithmV1
-	let endRange: numeric = 6
-	let startRange: numeric = 2
+	let algorithm: AlgorithmV1;
+	let endRange: number = 6;
+	let startRange: number = 2;
 
-	onMount(() => {
-		algoV1 = new AlgorithmV1()
-	})
-
+	function initAlgorithm() {
+		algorithm = new AlgorithmV1([endRange, startRange]);
+		const step = algorithm.step();
+		// alert(algorithm.step());
+	}
 </script>
 
 <h2>Retraction Distance</h2>
 
-<div class="h-2"></div>
+<div class="h-2" />
 
 <Row crossAxisAlignment="center">
 	<div>
 		<Column>
-			<NumberInput bind:value= {endRange} label="End Range in mm" />
-			<div class="h-2"></div>
-			<NumberInput bind:value= {startRange} label="Start Range in mm" />
+			<NumberInput bind:value={endRange} label="End Range in mm" />
+			<div class="h-2" />
+			<NumberInput bind:value={startRange} label="Start Range in mm" />
 		</Column>
-		<div class="h-4"></div>
-		<Button on:click={() => alert(`${endRange}`)}>Generate gcode</Button>
+		<div class="h-4" />
+		<Button on:click={initAlgorithm}>Generate gcode</Button>
 	</div>
 	<div class="w-96">
-		<img src="retraction_diagram.jpeg" alt="">
+		<img src="retraction_diagram.jpeg" alt="" />
 	</div>
 </Row>
