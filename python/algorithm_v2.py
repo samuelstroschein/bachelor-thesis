39beb7fb89b5ef12d3f045262d3a2b2639b2a202
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-04-17 18:17:39 +0200

    .

--- python/algorithm_v2.py
+++ python/algorithm_v2.py
@@ -7,43 +7,62 @@ prime_speed = range(30,60)
 # %%
 import nevergrad as ng
 import numpy as np
+from bayes_opt import BayesianOptimization
+
 
 RETRACTION_DISTANCE = range(2, 11)
 RETRACTION_SPEED = range(30, 61)
 PRIME_SPEED = range(30, 61)
 
 TRUTH_VALUE = np.array([
-    3,   # retraction_distance
+    30,  # retraction_distance is 3 but * 10 to be equal to other parameters
     30,  # retraction_speed
     40   # prime_speed
 ])
 
 
-def objective_function(x: np.ndarray) -> float:
+def objective_function_vector(x: np.ndarray) -> float:
     """
     Caluclates the absolute distance of x to the TRUTH_VALUE.
     """
-    return abs(np.sum(np.subtract(TRUTH_VALUE, x)))
+    return -1 * abs(np.sum(np.subtract(TRUTH_VALUE, x)))
 
+
+def objective_function_variables(x: float, y: float, z: float) -> float:
+    """
+    Caluclates the absolute distance of x to the TRUTH_VALUE.
+    """
+    return -1 * abs(np.sum(np.subtract(TRUTH_VALUE, np.array([x * 10, y, z])))) 
 
 # %%
-# optimizer = ng.optimizers.NGOpt(parametrization=3, budget=100)
-optimizer = ng.optimizers.RandomSearch(parametrization=3, budget=1000)
 
-# retraction_distance constraint
-optimizer.parametrization.register_cheap_constraint(
-    lambda x: RETRACTION_DISTANCE[0] <= x[0])
-# optimizer.parametrization.register_cheap_constraint(
-#     lambda x: RETRACTION_DISTANCE[0] <= x[0] <= RETRACTION_DISTANCE[-1])
-# # retraction_speed constraint
-# optimizer.parametrization.register_cheap_constraint(
-#     lambda x: RETRACTION_DISTANCE[0] <= x[1] <= RETRACTION_DISTANCE[-1])
-# # prime_speed constraint
-# optimizer.parametrization.register_cheap_constraint(
-#     lambda x: RETRACTION_DISTANCE[0] <= x[2] <= RETRACTION_DISTANCE[-1])
+pbounds = {'x': (2, 10), 'y': (30, 60), 'z': (30, 60)}
 
-recommendation = optimizer.minimize(
-    objective_function=objective_function)  # best value
-print(recommendation.value)
+optimizer = BayesianOptimization(
+    f=objective_function_variables,
+    pbounds=pbounds,
+    verbose=2,  # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+    random_state=1,
+)
+
+# set first initial point
+optimizer.probe(
+    params=[2, 30,30],
+    lazy=True,
+)
+
+# set second initial point
+optimizer.probe(
+    params=[10, 60, 60],
+    lazy=True,
+)
+
+optimizer.maximize(
+    init_points=0,
+    n_iter=8,
+    kappa=1.75
+)
+
+print(optimizer.max)
 
 # %%
