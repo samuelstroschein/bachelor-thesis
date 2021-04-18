220f60cc79a7a4d58ae199df7a13455c9764554b
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-04-18 13:09:15 +0200

    .

--- python/algorithm_v2_nevergrad.py
+++ python/algorithm_v2_nevergrad.py
@@ -1,7 +1,7 @@
 # %%
 import nevergrad as ng
 import numpy as np
-from python.nevergrad_algorithm_base import NevergradAlgorithmBase
+from nevergrad_algorithm_base import NevergradAlgorithmBase
 
 TRUTH_VALUE = np.array([
     3,  # retraction_distance is 3 but * 10 to be equal to other parameters
@@ -10,29 +10,26 @@ TRUTH_VALUE = np.array([
 ])
 
 
-def objective_function(x: np.ndarray) -> float:
+def objective_function(x: ng.p.Scalar,
+                       y: ng.p.Scalar,
+                       z: ng.p.Scalar,
+                       ) -> float:
     """
     Caluclates the absolute distance of x to the TRUTH_VALUE squared.
     """
     result = [
-        abs((TRUTH_VALUE[0]*10 - x[0]*10)) ** 2,
-        abs(TRUTH_VALUE[1] - x[1]) ** 2,
-        abs(TRUTH_VALUE[2] - x[2]) ** 2,
+        abs((TRUTH_VALUE[0]*10 - x*10)) ** 2,
+        abs(TRUTH_VALUE[1] - y) ** 2,
+        abs(TRUTH_VALUE[2] - z) ** 2,
     ]
     return abs(np.sum(result))
 
 
-def objective_function_variables(
-    x: ng.p.Scalar,
-    y: ng.p.Scalar,
-    z: ng.p.Scalar
-) -> float:
-    return objective_function(np.array([x, y, z]))
-
 # %%
 
+
 algorithm = NevergradAlgorithmBase(
-    objective_function_variables, ng.optimizers.NGOpt)
+    objective_function, ng.optimizers.NGOpt)
 
 
 for _ in range(20):  # type: ignore
