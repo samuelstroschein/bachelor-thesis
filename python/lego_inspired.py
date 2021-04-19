1a64b879097b6c38318c592a9d91cb302c1ac4b7
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-04-19 19:05:16 +0200

    added lego inspired optimizer

---
+++ python/lego_inspired.py
@@ -0,0 +1,45 @@
+from typing import List
+import nevergrad as ng
+from nevergrad_algorithm_base import NevergradAlgorithmBase
+from objective_functions import objective_function_v2_lego
+
+algorithm = NevergradAlgorithmBase(
+    ng.families.ParametrizedBO(
+        utility_kind="ei",
+        utility_kappa=1,
+        utility_xi=0
+    )
+)
+
+# two initial steps to start ranking
+steps = [algorithm.step(), algorithm.step()]
+# initial ranking
+ranking = [[*steps[0].args], [*steps[1].args]]  # type: ignore
+# tell the initial ranking
+algorithm.tell_loss(
+    steps[0], objective_function_v2_lego(*steps[0].args, ranking))  # type: ignore
+algorithm.tell_loss(
+    steps[1], objective_function_v2_lego(*steps[1].args, ranking))  # type: ignore
+
+print(f'First step {steps[0].args}')  # type:ignore
+print(f'Second step {steps[1].args}')  # type:ignore
+
+for i in range(10):
+    steps.append(algorithm.step())
+    print(f"Next step: {steps[-1].args}")  # type:ignore
+    rank = int(input("Which rank does the new step has? "))
+    ranking.insert(rank, [*steps[-1].args])  # type: ignore
+    # reinitialize the algorithm (to apply new ranking)
+    algorithm = NevergradAlgorithmBase(
+        ng.families.ParametrizedBO(
+            utility_kind="ei",
+            utility_kappa=1,
+            utility_xi=0
+        )
+    )
+    # tell new ranking
+    for step in steps:
+        algorithm.tell_loss(step, objective_function_v2_lego(  # type: ignore
+            *step.args, ranking))  # type: ignore
+
+print(algorithm.optimizer.provide_recommendation().args)  # type:ignore
--- python/objective_functions.py
+++ python/objective_functions.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Union
 import nevergrad as ng
 import numpy as np
 
@@ -14,7 +14,7 @@ def objective_function_v1(x: Union[ng.p.Scalar, float],
                           z: Union[ng.p.Scalar, float],
                           ) -> float:
     """
-    Caluclates the absolute distance of all values to the TRUTH_VALUEs squared.
+    Calculates the absolute distance of all values to the TRUTH_VALUEs squared.
     Furthermore, multiplies x by 10 to get it to the same levels of y and z
     """
     result = [
@@ -23,3 +23,17 @@ def objective_function_v1(x: Union[ng.p.Scalar, float],
         abs(TRUTH_VALUE[2] - z) ** 2,
     ]
     return abs(np.sum(result))
+
+
+def objective_function_v2_lego(x: Union[ng.p.Scalar, float],
+                               y: Union[ng.p.Scalar, float],
+                               z: Union[ng.p.Scalar, float],
+                               ranking: List[List],
+                               ) -> float:
+    """
+    X,y,z are transformed into a list and then searched for in the
+    ranking list. The index position of the search is the returned value. 
+
+    E.g. [x,y,z], ranking = [[30,45,2], [x,y,z]] -> returns 1 
+    """
+    return ranking.index([x, y, z])
