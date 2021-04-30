176985eb35f98bc675179323459b8340e9e230cd
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-04-30 13:34:26 +0200

    updated NevergradAlgorithmBase

--- python/lego_inspired.py
+++ python/lego_inspired.py
@@ -1,46 +1,10 @@
 import nevergrad as ng
 from nevergrad.optimization.base import Optimizer
 from objective_functions import objective_function_v2_lego
+from nevergrad_algorithm_base import NevergradAlgorithmBase
 
 
-class LegoBase:
-    def __init__(self,
-                 optimizer,
-                 temperature_range: range = range(180, 221, 5),
-                 retraction_distance_range: range = range(2, 11, 1),
-                 retraction_speed_range: range = range(30, 61, 10),
-                 #  prime_speed_range: range = range(30, 60, 10),
-                 ) -> None:
-        """
-        The ranges define the discret values for each parameter.
-
-        E.g. the temperature range may lay between 180,221,10 which means
-        each suggested temperature is either 180,190,200,210,220.
-        """
-        instrumentation = instrumentation = ng.p.Instrumentation(
-            ng.p.TransitionChoice(temperature_range),
-            ng.p.TransitionChoice(retraction_distance_range),
-            ng.p.TransitionChoice(retraction_speed_range),
-            # ng.p.TransitionChoice(prime_speed_range),
-        )
-        self.optimizer = optimizer(
-            parametrization=instrumentation,
-            budget=5,
-            num_workers=1
-        )
-
-    def step(self) -> Optimizer:
-        x = self.optimizer.ask()
-        return x
-        # # print(x.args)
-        # loss = self.objective_function(*x.args)
-        # self.optimizer.tell(x, loss)
-
-    def tell_loss(self, x: Optimizer, loss: float) -> None:
-        self.optimizer.tell(x, loss)
-
-
-algorithm = LegoBase(
+algorithm = NevergradAlgorithmBase(
     ng.families.ParametrizedBO(
         utility_kind="ei",
         utility_kappa=1,
@@ -67,7 +31,7 @@ for i in range(10):
     rank = int(input("Which rank does the new step has? "))
     ranking.insert(rank, [*steps[-1].args])  # type: ignore
     # reinitialize the algorithm (to apply new ranking)
-    algorithm = LegoBase(
+    algorithm = NevergradAlgorithmBase(
         ng.families.ParametrizedBO(
             utility_kind="ei",
             utility_kappa=1,
--- python/nevergrad_algorithm_base.py
+++ python/nevergrad_algorithm_base.py
@@ -6,28 +6,22 @@ from typing import Tuple
 class NevergradAlgorithmBase:
     def __init__(self,
                  optimizer,
-                 temperature_range: Tuple[float, float] = (180, 220),
-                 retraction_distance_range: Tuple[float, float] = (2, 10),
-                 retraction_speed_range: Tuple[float, float] = (30, 60),
-                 #  prime_speed_range: Tuple[float, float] = (30, 60),
+                 temperature_range: range = range(180, 221, 5),
+                 retraction_distance_range: range = range(2, 11, 1),
+                 retraction_speed_range: range = range(30, 61, 10),
+                 #  prime_speed_range: range = range(30, 60, 10),
                  ) -> None:
+        """
+        The ranges define the discret values for each parameter.
+
+        E.g. the temperature range may lay between 180,221,10 which means
+        each suggested temperature is either 180,190,200,210,220.
+        """
         instrumentation = instrumentation = ng.p.Instrumentation(
-            ng.p.Scalar(
-                lower=temperature_range[0],
-                upper=temperature_range[1]
-            ).set_integer_casting(),
-            ng.p.Scalar(
-                lower=retraction_distance_range[0],
-                upper=retraction_distance_range[1]
-            ).set_integer_casting(),
-            ng.p.Scalar(
-                lower=retraction_speed_range[0],
-                upper=retraction_speed_range[1]
-            ).set_integer_casting(),
-            # ng.p.Scalar(
-            #     lower=prime_speed_range[0],
-            #     upper=prime_speed_range[1]
-            # ).set_integer_casting(),
+            ng.p.TransitionChoice(temperature_range),
+            ng.p.TransitionChoice(retraction_distance_range),
+            ng.p.TransitionChoice(retraction_speed_range),
+            # ng.p.TransitionChoice(prime_speed_range),
         )
         self.optimizer = optimizer(
             parametrization=instrumentation,
