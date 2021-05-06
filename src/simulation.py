d75f20a75643d263f5dc0a837f2bf59ad111dd20
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-05-06 07:23:07 +0200

    increased epochs

--- src/simulation.py
+++ src/simulation.py
@@ -131,7 +131,7 @@ def experiment(
                             ),
                             truth_value=random_truth_value,
                             bounds=bounds,
-                            epochs=10
+                            epochs=20
                         )
                         best_point = simulation.parameters_to_array(
                             simulation.max['params']
